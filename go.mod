d67f792722b9fdb002c737a83ce1965aa5123574
Wendell Sun <iwendellsun@gmail.com>
2021-06-11 19:00:00 +0800

    Update callback logic

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/xwjdsh/hydra-auth-demo
 go 1.16
 
 require (
+	github.com/NYTimes/gziphandler v1.1.1 // indirect
 	github.com/gin-gonic/gin v1.7.2
+	github.com/gorilla/csrf v1.7.0
+	github.com/justinas/nosurf v1.1.1 // indirect
 	github.com/ory/hydra-client-go v1.10.3
+	github.com/turtlemonvh/gin-wraphh v0.0.0-20160304035037-ea8e4927b3a6
 )
--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/csrf"
 	"github.com/ory/hydra-client-go/client"
 	"github.com/ory/hydra-client-go/client/admin"
 	"github.com/ory/hydra-client-go/models"
+	wraphh "github.com/turtlemonvh/gin-wraphh"
 )
 
 type handler struct {
@@ -53,7 +55,7 @@ func initHandler(adminUrl, publicUrl string) (*handler, error) {
 		admin.NewCreateOAuth2ClientParams().WithBody(&models.OAuth2Client{
 			ClientID:     "auth-demo",
 			ClientSecret: "auth-demo",
-			RedirectUris: []string{"http://127.0.0.1:9000/login.html"},
+			RedirectUris: []string{"http://127.0.0.1:9000/callback"},
 			Scope:        "user product order admin",
 		}))
 	if err != nil {
@@ -67,10 +69,21 @@ func initHandler(adminUrl, publicUrl string) (*handler, error) {
 		r:           r,
 	}
 
+	csrfMw := csrf.Protect(
+		[]byte("csrc-key"),
+		csrf.SameSite(csrf.SameSiteLaxMode),
+		csrf.Secure(false),
+	)
+	r.Use(wraphh.WrapHH(csrfMw))
+
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
+	r.GET("/login", h.loginPage)
+
+	r.GET("/callback", h.callback)
+	r.GET("/consent", h.consentPage)
 
 	r.POST("/login", h.login)
 	r.POST("/consent", h.consent)
@@ -109,7 +122,7 @@ func (h *handler) login(c *gin.Context) {
 		return
 	}
 
-	resp, err := h.hydraAdmin.Admin.GetLoginRequest(
+	_, err := h.hydraAdmin.Admin.GetLoginRequest(
 		admin.NewGetLoginRequestParams().WithLoginChallenge(loginChallenge),
 	)
 	if err != nil {
@@ -117,18 +130,72 @@ func (h *handler) login(c *gin.Context) {
 		return
 	}
 
-	c.HTML(http.StatusFound, "consent.html", gin.H{
-		"ClientID":       resp.Payload.Client.ClientID,
-		"RequestedScope": resp.Payload.RequestedScope,
-		"LoginChallenge": loginChallenge,
+	username := req.Username
+	resp, err := h.hydraAdmin.Admin.AcceptLoginRequest(
+		admin.NewAcceptLoginRequestParams().WithBody(&models.AcceptLoginRequest{
+			Remember: true,
+			Subject:  &username,
+		}).WithLoginChallenge(loginChallenge),
+	)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.Redirect(http.StatusFound, *resp.Payload.RedirectTo)
+}
+
+func (h *handler) consentPage(c *gin.Context) {
+	challenge := c.Query("consent_challenge")
+	if challenge == "" {
+		c.Redirect(http.StatusFound, "/index")
+		return
+	}
+
+	resp, err := h.hydraAdmin.Admin.GetConsentRequest(
+		admin.NewGetConsentRequestParams().WithConsentChallenge(challenge),
+	)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.HTML(http.StatusOK, "consent.html", gin.H{
+		"ClientID":         resp.Payload.Client.ClientID,
+		"RequestedScope":   resp.Payload.RequestedScope,
+		"ConsentChallenge": challenge,
 	})
 }
 
 func (h *handler) consent(c *gin.Context) {
-	consent := c.Query("login_challenge")
+	consent := c.Query("consent_challenge")
 	if consent == "" {
-		c.HTML(http.StatusOK, "login.html", nil)
+		c.Status(http.StatusInternalServerError)
 		return
 	}
-	c.String(http.StatusOK, "OK")
+
+	resp, err := h.hydraAdmin.Admin.AcceptConsentRequest(
+		admin.NewAcceptConsentRequestParams().WithBody(&models.AcceptConsentRequest{
+			GrantScope: c.Request.Form["scope"],
+			Remember:   true,
+		}),
+	)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.Redirect(http.StatusFound, *resp.Payload.RedirectTo)
+}
+
+func (h *handler) callback(c *gin.Context) {
+	c.String(http.StatusOK, "callback")
+}
+
+func (h *handler) loginPage(c *gin.Context) {
+	challenge := c.Query("login_challenge")
+	c.HTML(http.StatusOK, "login.html", gin.H{
+		"LoginChallenge": challenge,
+		csrf.TemplateTag: csrf.TemplateField(c.Request),
+	})
 }
